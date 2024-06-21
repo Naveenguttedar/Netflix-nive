676f2d23327266ad1a6549275266dc48e804ce8a
Naveen R guttedar <naveen@Naveens-MacBook-Air.local>
2024-06-22 00:18:36 +0530

    added data.json

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -6,14 +6,18 @@ import Footer from "../components/Footer";
 const Hero = () => {
   const [_Data, setData] = useState([]);
   useEffect(() => {
-    axios.get("../data.json").then((response) => setData(response.data));
+    axios
+      .get(
+        "https://github.com/Naveenguttedar/Netflix-nive/blob/master/data.json",
+      )
+      .then((response) => setData(response.data));
+    console.log(_Data);
   }, []);
   return (
     <div>
       <Main />
       <section>
         {_Data.map((movie_row, i) => {
-          console.log(movie_row.movies);
           return (
             <Row
               rowId={i}
